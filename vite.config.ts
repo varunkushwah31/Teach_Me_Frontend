bdff87c617d11644229da26e3e69262c476fd903
Varun Kushwah <varun.kush3@gmail.com>
2026-06-18 23:42:02 +0530

    integrate Tailwind CSS and set up routing with React Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+      },
+    },
+  },
 })
